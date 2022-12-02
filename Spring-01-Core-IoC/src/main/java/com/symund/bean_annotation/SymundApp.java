911c522a868ed36e13819994e473d29b3b9aaa57
mziya <mziyag5187@gmail.com>
2022-12-02 15:08:32 +0300

    Multiple Beans Same Type

--- Spring-01-Core-IoC/src/main/java/com/symund/bean_annotation/SymundApp.java
+++ Spring-01-Core-IoC/src/main/java/com/symund/bean_annotation/SymundApp.java
@@ -10,6 +10,11 @@ public class SymundApp {
 
         FullTimeMentor ft = container.getBean(FullTimeMentor.class);
 
+        //PartTimeMentor pt = container.getBean("p1",PartTimeMentor.class);
+
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+        pt.createAccount();
+
         ft.createAccount();
 
         String str = container.getBean(String.class);
