e491e30de6dd2b2ade6bb71e7b4fc7db4db89953
mziya <mziyag5187@gmail.com>
2022-12-03 11:33:13 +0300

    Dependency Injection

---
+++ Spring-03-Core-DI/src/main/java/com/symund/SymundApp.java
@@ -0,0 +1,16 @@
+package com.symund;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SymundApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+        Java java = container.getBean(Java.class);
+        java.getTeachingHours();
+
+
+    }
+}
