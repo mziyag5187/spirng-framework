2d4137fec841c92130a19c3fea9186a9edec0ec9
mziya <mziyag5187@gmail.com>
2022-12-04 18:06:35 +0300

    Spring Boot Demo

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/symund/SymundApplication.java
@@ -0,0 +1,13 @@
+package com.symund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymundApplication.class, args);
+    }
+
+}
---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/symund/SymundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
