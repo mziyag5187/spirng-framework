7da9f2bd042981130f7c53f8a4a09fc745440802
mziya <mziyag5187@gmail.com>
2022-12-07 11:43:41 +0300

    @RequestMapping at class level

--- Spring-08-Spring-MVC-Model/src/main/java/com/symund/controller/AnotherController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/symund/controller/AnotherController.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mentor")
-public class MentorController {
+@RequestMapping("/another")
+public class AnotherController {
 
-    @RequestMapping("/list") //localhost:8080/mentor/list
+    @RequestMapping("/list") //localhost:8080/another/list
     public String showTable(Model model){
 
         List<Mentor> mentorList = new ArrayList<>();
-        mentorList.add(new Mentor("Mike","Smith",45,Gender.MALE));
+        mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
         mentorList.add(new Mentor("Tom","Hanks",65,Gender.MALE));
         mentorList.add(new Mentor("Ammy","Bryan",25,Gender.MALE));
 
