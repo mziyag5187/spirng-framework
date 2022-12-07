688eb83b609d5464dcb9402a119e305990998cf7
mziya <mziyag5187@gmail.com>
2022-12-07 11:31:44 +0300

    Spring MVC - Mentor task

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/symund/controller/MentorController.java
@@ -0,0 +1,28 @@
+package com.symund.controller;
+
+import com.symund.enums.Gender;
+import com.symund.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String mentor(Model model){
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike","Smith",45,Gender.MALE));
+        mentorList.add(new Mentor("Tom","Hanks",65,Gender.MALE));
+        mentorList.add(new Mentor("Ammy","Bryan",25,Gender.MALE));
+
+        model.addAttribute("mentors", mentorList);
+
+
+        return "mentor/mentor-list";
+    }
+}
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/symund/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.symund.enums;
+
+public enum Gender {
+    MALE,FEMALE;
+}
