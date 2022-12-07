7ec8bba86b0fa776f67039418297af1bd1052c0b
mziya <mziyag5187@gmail.com>
2022-12-07 16:28:05 +0300

    Spring Thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/test/java/com/symund/spring09springthymeleaf/Spring09SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symund.spring09springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
