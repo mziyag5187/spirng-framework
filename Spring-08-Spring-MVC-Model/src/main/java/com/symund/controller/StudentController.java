76747335edd4516f06173c32c0ad87da205c73de
mziya <mziyag5187@gmail.com>
2022-12-07 10:46:37 +0300

    Spring MVC - Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/symund/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/symund/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.symund.controller;
 
+import com.symund.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -12,6 +17,25 @@ public class StudentController {
 
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in your UI
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("studentId", studentId );
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student", student);
+
+
 
         return "student/welcome";
     }
