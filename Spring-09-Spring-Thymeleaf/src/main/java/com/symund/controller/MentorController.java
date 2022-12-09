22f70ea22551c72a5bed65c230b9d04d2af90ff3
mziya <mziyag5187@gmail.com>
2022-12-09 11:33:47 +0300

    Spring Thymeleaf - redirecting, capturing the data from UI

--- Spring-09-Spring-Thymeleaf/src/main/java/com/symund/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/symund/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.symund.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -21,6 +23,13 @@ public class MentorController {
         model.addAttribute("batchList", batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor, Model model){
+
+        //return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 
 }
